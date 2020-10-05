dcbda4089f6362e4c71d1d9c49ec82d04871821b
JunyaAmano <amanojaku.jr@gmail.com>
2020-10-05 07:03:42 +0000

    Finish Ajax

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,17 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    # redirect_to book_path(book)
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.post_comments.find(params[:id])
+    book_comment.destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,5 @@ class Favorite < ApplicationRecord
   validates :book_id, {presence: true}
   belongs_to :book
   belongs_to :user
+  validates_uniqueness_of :book_id, scope: :user_id
 end
